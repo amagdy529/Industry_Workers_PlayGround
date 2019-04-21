27d3a1b680f7aca666875f232b5f11f823f0fc45
amagdy529 <amagdy529@gmail.com>
2019-04-22 01:17:41 +0200

    new folder

--- admin/footer.php
+++
@@ -1,8 +0,0 @@
-
-
-<!-- <div class=" row  text-center" background="Black">
-      <div class="well">Insititute of Information and Technology </div>
-</div> -->
-
-</body>
-</html>
--- admin/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// require_once "../controllers/DbConnection.php";
-require_once "header.php";
-// echo "hello inside index";
-require_once "footer.php";
-?>
