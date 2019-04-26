7b0e7e93b8f0e14983aeb05f4a415681a87e830a
amagdy529 <amagdy529@gmail.com>
2019-04-26 18:28:06 +0200

    front footer , front header

---
+++ craftsmen/front-footer.php
@@ -0,0 +1,86 @@
+<footer class="footer" style="background-image: url('images/footer-bg.png');">
+    <div class="inner">
+        <div class="top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img src="images/logo-white.png" alt="constructor" class="footer-logo">
+                        <p>There ain't nothin' wrong with that. Just sit right back and you'll hear a tale a tale of
+                            a fateful trip that started from this tropic port aboard this tiny ship. Here he comes
+                            Here comes Speed Racer. He's a demon on wheels. And we know Flipper lives in a world
+                            full .</p>
+                        <div class="footer-social-icons">
+                            <a href=""><i class="fa fa-facebook"></i></a>
+                            <a href=""><i class="fa fa-twitter"></i></a>
+                            <a href=""><i class="fa fa-linkedin"></i></a>
+                            <a href=""><i class="fa fa-google-plus"></i></a>
+                        </div> <!-- end .footer-social-icons -->
+                    </div> <!-- end .col-sm-3 -->
+                    <div class="col-sm-3">
+                        <h4>Contact Information</h4>
+                        <div class="footer-contact clearfix">
+                            <i class="icon-pointer"></i>
+                            <div class="content">09 Design Street, Downtown, Sydney, Australia</div>
+                        </div> <!-- end .footer-contact -->
+                        <div class="footer-contact clearfix">
+                            <i class="icon-call-end"></i>
+                            <div class="content">+01 123 456 78</div>
+                        </div> <!-- end .footer-contact -->
+                        <div class="footer-contact clearfix">
+                            <i class="icon-printer"></i>
+                            <div class="content">+01 123 456 78</div>
+                        </div> <!-- end .footer-contact -->
+                        <div class="footer-contact clearfix">
+                            <i class="icon-envelope"></i>
+                            <div class="content">Info@Constructor.com</div>
+                        </div> <!-- end .footer-contact -->
+                    </div> <!-- end .col-sm-3 -->
+                    <div class="col-sm-3">
+                        <h4>Working Hours</h4>
+                        <div class="footer-hours">Monday To Friday :<br>9:00 AM to 9:00 PM</div>
+                        <div class="footer-hours">Saturday :<br>9:00 AM to 2:00 PM</div>
+                        <div class="footer-hours">Sunday :<br>Holiday</div>
+                    </div> <!-- end .col-sm-3 -->
+                    <div class="col-sm-3">
+                        <h4>Our Location</h4>
+                        <div class="footer-map" id="footer-map"></div>
+                    </div> <!-- end .col-sm-3 -->
+                </div> <!-- end .row -->
+                <hr>
+            </div> <!-- end .container -->
+        </div> <!-- end .top -->
+        <div class="bottom">
+            <div class="container">
+                <span>Copyright © 2017 - All Rights Reserved</span>
+
+                <div id="back-to-top">
+                    <a href="#top">go to Top <i class="fa fa-caret-up"></i></a>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div> <!-- end .bottom -->
+    </div> <!-- end .inner -->
+</footer> <!-- end .footer -->
+
+<!-- jQuery -->
+<script src="js/jquery-1.11.2.min.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- google maps -->
+<script type='text/javascript'
+    src='https://maps.googleapis.com/maps/api/js?key=AIzaSyAZsDkJFLS0b59q7cmW0EprwfcfUA8d9dg'></script>
+<!-- FlexSlider -->
+<script src="scripts/FlexSlider/jquery.flexslider-min.js"></script>
+<!-- Owl Carousel -->
+<script src="js/owl.carousel.min.js"></script>
+<!-- Isotope -->
+<script src="js/isotope.pkgd.min.js"></script>
+<script src="js/imagesloaded.pkgd.min.js"></script>
+<script src="js/jquery.magnific-popup.min.js"></script>
+
+<!-- Scripts.js -->
+<script src="js/scripts.js"></script>
+
+</body>
+
+</html>
--- craftsmen/front-header.php
+++ craftsmen/front-header.php
@@ -1,102 +1,157 @@
 <!DOCTYPE html>
 <html lang="en">
-	<head>
 
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>CRAFTSMAN - Carpentry HTML Template</title>
+<head>
 
-		<!-- Fonts -->
-		<link href="https://fonts.googleapis.com/css?family=Cabin:700%7CExo+2:400,500,600,700%7CLato:300,400,700%7COxygen%7CRoboto:300,400,500,700" rel="stylesheet">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CRAFTSMAN - Carpentry HTML Template</title>
 
-		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<!-- Font Awesome -->
-		<link href="fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="css/settings.css" media="screen" />
-		<!-- Simple-Line-Icons-Webfont -->
-		<link href="fonts/Simple-Line-Icons-Webfont/simple-line-icons.css" rel="stylesheet">
-		<!-- constructor Icons -->
-		<link href="fonts/constructor-icons/style.css" rel="stylesheet">
-		<!-- FlexSlider -->
-		<link href="scripts/FlexSlider/flexslider.css" rel="stylesheet">
-		<link href="css/magnific-popup.css" rel="stylesheet">
-		<!-- Owl Carousel -->
-		<link href="css/owl.carousel.css" rel="stylesheet">
-		<link href="css/owl.theme.default.css" rel="stylesheet">
-		<!-- Style.css -->
-		<link href="css/icons.css" rel="stylesheet">
-		<link href="css/style.css" rel="stylesheet">
+    <!-- Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Cabin:700%7CExo+2:400,500,600,700%7CLato:300,400,700%7COxygen%7CRoboto:300,400,500,700"
+        rel="stylesheet">
 
-    	<link rel="icon" href="images/favicon.png" sizes="32x32" />
-	</head>
-	<body>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="fonts/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/settings.css" media="screen" />
+    <!-- Simple-Line-Icons-Webfont -->
+    <link href="fonts/Simple-Line-Icons-Webfont/simple-line-icons.css" rel="stylesheet">
+    <!-- constructor Icons -->
+    <link href="fonts/constructor-icons/style.css" rel="stylesheet">
+    <!-- FlexSlider -->
+    <link href="scripts/FlexSlider/flexslider.css" rel="stylesheet">
+    <link href="css/magnific-popup.css" rel="stylesheet">
+    <!-- Owl Carousel -->
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.default.css" rel="stylesheet">
+    <!-- Style.css -->
+    <link href="css/icons.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <style>
+    .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+    }
 
-		<header class="header header-landing">
+    @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+            font-size: 3.5rem;
+        }
+    }
 
-			<div class="site-top hidden-xs hidden-sm">
-                <div class="container">
-                    <div class="row middle">
-                        <div class="col-md-12 col-lg-7">
-                            <aside id="text-2" class="widget widget_text">
-                                <div class="textwidget">
-                                    <ul class="extra-info">
-                                        <li><i class="fa fa-phone"> </i> (102) 6666 8888</li>
-                                        <li><i class="fa fa-clock-o"></i> Mon - Sat: 9:00 - 18:00</li>
-                                        <li><i class="fa fa-envelope-o"></i> info@craftsman.com</li>
-                                    </ul>
-                                </div>
-                            </aside>
-                        </div>
-                        <div class="col-lg-5 hidden-md end-lg">
-                            <div class="social-menu">
-                                <ul id="social-menu-top" class="menu">
-                                    <li><a href="http://facebook.com/">facebook</a></li>
-                                    <li><a href="http://twitter.com/">twitter</a></li>
-                                    <li><a href="http://linkedin.com/">linkedin</a></li>
-                                    <li><a href="http://dribbble.com/">dribbble</a></li>
-                                    <li><a href="http://youtube.com/">youtube</a></li>
-                                    <li><a href="#">feed</a></li>
+    .jumbotron {
+        padding-top: 3rem;
+        padding-bottom: 3rem;
+        margin-bottom: 0;
+        background-color: #fff;
+    }
+
+    @media (min-width: 768px) {
+        .jumbotron {
+            padding-top: 6rem;
+            padding-bottom: 6rem;
+        }
+    }
+
+    .jumbotron p:last-child {
+        margin-bottom: 0;
+    }
+
+    .jumbotron-heading {
+        font-weight: 300;
+    }
+
+    .jumbotron .container {
+        max-width: 40rem;
+    }
+
+    footer {
+        padding-top: 3rem;
+        padding-bottom: 3rem;
+    }
+
+    footer p {
+        margin-bottom: .25rem;
+    }
+    </style>
+    <link rel="icon" href="images/favicon.png" sizes="32x32" />
+</head>
+
+<body>
+
+    <header class="header header-landing">
+
+        <div class="site-top hidden-xs hidden-sm">
+            <div class="container">
+                <div class="row middle">
+                    <div class="col-md-12 col-lg-7">
+                        <aside id="text-2" class="widget widget_text">
+                            <div class="textwidget">
+                                <ul class="extra-info">
+                                    <li><i class="fa fa-phone"> </i> (102) 6666 8888</li>
+                                    <li><i class="fa fa-clock-o"></i> Mon - Sat: 9:00 - 18:00</li>
+                                    <li><i class="fa fa-envelope-o"></i> info@craftsman.com</li>
                                 </ul>
                             </div>
+                        </aside>
+                    </div>
+                    <div class="col-lg-5 hidden-md end-lg">
+                        <div class="social-menu">
+                            <ul id="social-menu-top" class="menu">
+                                <li><a href="http://facebook.com/">facebook</a></li>
+                                <li><a href="http://twitter.com/">twitter</a></li>
+                                <li><a href="http://linkedin.com/">linkedin</a></li>
+                                <li><a href="http://dribbble.com/">dribbble</a></li>
+                                <li><a href="http://youtube.com/">youtube</a></li>
+                                <li><a href="#">feed</a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-            
-			<div class="container clearfix">
-				<div class="logo"><a href="index.html"><img src="images/logo.png" alt="constructor" class="img-responsive"></a></div> <!-- end .logo -->
-				<div class="navigation clearfix">
-					<nav class="main-nav">
-						<ul class="list-unstyled">
-							<li><a href="index.html">Home</a></li>
-							<li>
-								<a href="03-About-Us.html">About Us</a>
-								<ul>
-									<li><a href="11-404.html">404</a></li>
-								</ul>
-							</li>
-							<li>
-								<a href="04-Services.html">Services</a>
-								<ul>
-									<li><a href="08-Service-Details.html">Service Details</a></li>
-								</ul>
-							</li>
-							<li class="active"><a href="07-Gallery.html">Portfolio</a></li>
-							<li class="active"><a href="workers-list.php">Workers</a></li>
-							<li>
-								<a href="06-Blog-02.html">Blog</a>
-								<ul>
-									<li><a href="06-Blog-02.html">Blog</a></li>
-									<li><a href="05-Blog-01.html">Blog Grid</a></li>
-									<li><a href="09-Single-Post.html">Single Post</a></li>
-								</ul>
-							</li>
-							<li><a href="12-Contact-Us.html">Contact Us</a></li>
-						</ul>
-					</nav> <!-- end .main-nav -->
-					<a href="" class="responsive-menu-open"><i class="fa fa-bars"></i></a>
-				</div> <!-- end .navigation -->
-			</div> <!-- end .container -->
-		</header> <!-- end .header -->
+        </div>
+
+        <div class="container clearfix">
+            <div class="logo"><a href="index.html"><img src="images/logo.png" alt="constructor"
+                        class="img-responsive"></a></div> <!-- end .logo -->
+            <div class="navigation clearfix">
+                <nav class="main-nav">
+                    <ul class="list-unstyled">
+                        <li><a href="index.html">Home</a></li>
+                        <li>
+                            <a href="03-About-Us.html">About Us</a>
+                            <ul>
+                                <li><a href="11-404.html">404</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <a href="04-Services.html">Services</a>
+                            <ul>
+                                <li><a href="08-Service-Details.html">Service Details</a></li>
+                            </ul>
+                        </li>
+                        <li class="active"><a href="07-Gallery.html">Portfolio</a></li>
+                        <li class="active"><a href="workers-list.php">Workers</a></li>
+                        <li>
+                            <a href="06-Blog-02.html">Blog</a>
+                            <ul>
+                                <li><a href="06-Blog-02.html">Blog</a></li>
+                                <li><a href="05-Blog-01.html">Blog Grid</a></li>
+                                <li><a href="09-Single-Post.html">Single Post</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="12-Contact-Us.html">Contact Us</a></li>
+                    </ul>
+                </nav> <!-- end .main-nav -->
+                <a href="" class="responsive-menu-open"><i class="fa fa-bars"></i></a>
+            </div> <!-- end .navigation -->
+        </div> <!-- end .container -->
+    </header> <!-- end .header -->
