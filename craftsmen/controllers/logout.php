1ad8b0a61bc0bba5e651d96defe1e800521a21ad
amagdy529 <amagdy529@gmail.com>
2019-04-27 12:29:37 +0200

    sessions destry in logout

--- craftsmen/controllers/logout.php
+++ craftsmen/controllers/logout.php
@@ -2,11 +2,12 @@
 session_start();
 setcookie("login","",time()-3600,"/");
 unset($_SESSION['u_id']);
+unset($_SESSION['user_type']);
 unset($_COOKIE['logged_user_id']);
 unset($_COOKIE['logged_user_type']);
-// print_r($_COOKIE);
-// die;				
-// $_SESSION['u_id']=$row['u_id'];
-// die('inside logout');
-header("location: ../../craftsmen/index.html");
+// remove all session variables
+session_unset(); 
+// destroy the session 
+session_destroy(); 
+header("location: ../../craftsmen/index.php");
 ?>
